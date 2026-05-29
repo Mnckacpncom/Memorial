bc8e0ae464e6cce4a1711b88753b544b1623c601
Work <mnckacpncom@gamil.com>
2026-05-29 16:25:44 +0800

    初始项目

---
+++ src/components/PageRecords.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+	name: "PageRecords",
+	data: function() {
+		return {
+			tableData: [{
+				id: 1,
+				company: '恒昇',
+				Name:'张三',
+				Type,
+				Mode,
+				inTime,
+				outTime,
+				AccessCard,
+				AccessStatus,
+				EmployeeID,
+				IDCardStatus,
+				PrincipalPersoninCharge}
+			]
+		}
+	}
+}
+</script>
+
+<template>
+	<el-table :data="tableData">
+		<el-table-column></el-table-column>
+		<el-table-column></el-table-column>
+		<el-table-column></el-table-column>
+		<el-table-column></el-table-column>
+		<el-table-column></el-table-column>
+	</el-table>
+</template>
+
+<style scoped>
+
+</style>
